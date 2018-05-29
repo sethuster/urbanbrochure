979a2da56f4e33c1ec110878630f095aeff9b4f5
Ezo <sethuster@gmail.com>
2018-05-29 09:34:08 -0600

    shittily working

---
+++ urban.js
@@ -0,0 +1,12 @@
+var text = ["Passionate", "Experianced", "Enthusiastic", "Professional"]
+var counter = 0;
+var elem = document.getElementById("flavor");
+var inst = setInterval(change, 30000)
+
+function change() {
+	elem.innerHTML = text[counter];
+	counter++;
+	if(counter >= text.length){
+		counter = 0;
+	}
+}
